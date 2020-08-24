559b4163b6693a08686626d708198df925525065
Sam Wolfe <Sam Wolfe@192.168.0.110>
2020-08-24 17:41:22 -0400

    Added themes and more formatting

--- src/bulletin/GalleryApp.java
+++ src/bulletin/GalleryApp.java
@@ -10,16 +10,13 @@ import javafx.scene.*;
 import javafx.event.*;
 import javafx.stage.*;
 import javafx.application.*;
-import javafx.geometry.*;
 import javafx.animation.*;
 import javafx.util.*;
 import java.net.URL;
-//import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.Runnable;
 import java.util.Random;
 import java.util.Arrays;
-//import java.net.URLEncoder;
+
 
 /**
  * Displays pins from a Pinterest board
@@ -31,11 +28,7 @@ public class GalleryApp extends Application {
 	private Scene scene;
 	private GridPane tile = new GridPane();;
 	private VBox container;
-	//private HBox biggerBox;
 	private HBox menu;
-	private HBox toolbar;
-	private TextField search;
-	private Button update;
 	private int count;
 	private int numResults;
 	private String input;
@@ -52,19 +45,16 @@ public class GalleryApp extends Application {
 	/** @inheritdoc */
 	@Override
 	public void start(Stage stage) {
-		// call create methods and add them to bigBox (the VBox)
 		this.stage = stage;
 		createMenu();						//createMenu()->createTheme(),createHelp()
-		createToolbar(); //createToolbar()->convertUrl()->urlTests()
+		convertUrl();						//convertUrl()->urlTests()
 		createProgressBar();
 		container = new VBox();
-		container.setPrefWidth(500);// prefWidth
-		container.setPrefHeight(550);// prefHeight
-		container.getChildren().addAll(menu, toolbar, tile, bar);
-
-		//biggerBox = new HBox();
-		//biggerBox.getChildren().add(bigBox);
+		container.setPrefWidth(500);		
+		container.setPrefHeight(540);
+		container.getChildren().addAll(menu,tile);
 		scene = new Scene(container);
+		scene.getStylesheets().add("file:src/resources/spots.css");  //the original "theme" is set to Tie Dye
 		stage.setMaxWidth(640);
 		stage.setMaxHeight(480);
 		stage.setTitle("My Bulletin");
@@ -80,24 +70,32 @@ public class GalleryApp extends Application {
 	public void createMenu() {
 		MenuButton menuButton = new MenuButton("File");
 		MenuItem exit = new MenuItem("Exit");
-		// create event to exit when button is clicked
-		EventHandler<ActionEvent> exiting = event -> System.exit(0);
+		EventHandler<ActionEvent> exiting = event -> System.exit(0); // create event to exit when button is clicked
 		exit.setOnAction(exiting);
 		MenuButton menuButton2 = new MenuButton("Help");
 		MenuButton menuButton3 = new MenuButton("Theme");
-		// call createTheme to built new menu
-		createTheme(menuButton3);
-		// call createHelp method to build new menu
-		createHelp(menuButton2);
+		createTheme(menuButton3);									// call createTheme to built scroll menu button
+		createInfo(menuButton2);									// call createInfo method to build new menu 
 		menuButton.getItems().addAll(exit);
+		
+		Button pauseButton = new Button("Pause");
+		EventHandler<ActionEvent> handle = event -> update(tile, array);
+		KeyFrame keyFrame = new KeyFrame(Duration.seconds(2), handle);  // updates the tile every 2 sec
+		Timeline timeline = new Timeline(); 							// sets up timeline that stops when pause is pressed
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.getKeyFrames().add(keyFrame);
+		timeline.play();
+		pauseButton.setOnAction(e -> play(timeline, pauseButton));
+		pauseButton.setTranslateX(250);
+		
 		menu = new HBox();
-		menu.getChildren().addAll(menuButton, menuButton3, menuButton2);
+		menu.getChildren().addAll(menuButton, menuButton3, menuButton2, pauseButton);
 	}
 
 	/**
-	 * Method to create the extra credit help menu. The help menu has a about item.
+	 * Method to create an info pop-up with a bio and contact information.
 	 */
-	public void createHelp(MenuButton menuButton2) {
+	public void createInfo(MenuButton menuButton2) {
 		MenuItem about = new MenuItem("About");
 		VBox samBox = new VBox();
 		Image samPic = new Image("https://pbs.twimg.com/profile_images/" + "1114024663418580993/TsVZiMqp_400x400.jpg");
@@ -106,8 +104,7 @@ public class GalleryApp extends Application {
 		Text email = new Text("sgw73466@uga.edu");
 		Text vers = new Text("Version 678314");
 		samBox.getChildren().addAll(samView, name, email, vers);
-		// when clicked, menuitem opens new window
-		EventHandler<ActionEvent> helping = event -> {
+		EventHandler<ActionEvent> helping = event -> {    			// when clicked, menu item opens new window
 			Scene scene = new Scene(samBox);
 			Stage stage = new Stage();
 			stage.setTitle("About Sam Wolfe:");
@@ -124,12 +121,21 @@ public class GalleryApp extends Application {
 	 * Method to create add a basic and edgy theme
 	 */
 	public void createTheme(MenuButton menuButton3) {
-		MenuItem theme = new MenuItem("Edgy");
-		MenuItem basic = new MenuItem("Basic");
+		MenuItem theme = new MenuItem("Space");
+		MenuItem basic = new MenuItem("Tie Dye");
+		MenuItem spots = new MenuItem("Spots");
 		theme.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-			public void handle(ActionEvent e) {// call css file
-				scene.getStylesheets().add("file:src/bulletin/stylesheet.css");
+			public void handle(ActionEvent e) {					// call css file
+				scene.getStylesheets().add("file:src/resources/space.css");
+				stage.setScene(scene);
+				stage.show();
+			}
+		});
+		spots.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {					// call css file
+				scene.getStylesheets().add("file:src/resources/spots.css");
 				stage.setScene(scene);
 				stage.show();
 			}
@@ -137,47 +143,15 @@ public class GalleryApp extends Application {
 		basic.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				scene.getStylesheets().clear();// set to basic theme
+				scene.getStylesheets().add("file:src/resources/stylesheet.css");
 				stage.setScene(scene);
 				stage.show();
 			}
 		});
-		menuButton3.getItems().addAll(theme, basic);
+		menuButton3.getItems().addAll(basic,theme,spots);
 	}
 
-	/**
-	 * Method to build the toolbar with pause, search query, and update.
-	 * createToolbar() sets up the timeline used for pause/play button
-	 */
-	public void createToolbar() {
-		Button pauseButt = new Button("pause");
-		EventHandler<ActionEvent> handle = event -> update(tile, array);
-		KeyFrame keyFrame = new KeyFrame(Duration.seconds(2), handle);// updates the tile every 2 sec
-		Timeline timeline = new Timeline(); // sets up timeline that stops when pause is pressed
-		timeline.setCycleCount(Timeline.INDEFINITE);
-		timeline.getKeyFrames().add(keyFrame);
-		timeline.play();
-		pauseButt.setOnAction(e -> play(timeline, pauseButt));
-		//Label query = new Label("Search Query");
-		//search = new TextField("rock");
-		/*Button update = new Button("Update Images");
-		input2 = "rock";// default search query text
-		update.setOnAction(e -> {
-			Runnable r = () -> {
-				pUpdate(0);
-				// convertUrl(search,tile);
-			};
-			Thread t = new Thread(r);
-			t.setDaemon(true);
-			t.start();
-		});*/
-		convertUrl();//converts url to string
-		toolbar = new HBox();
-		toolbar.setPadding(new Insets(10, 12, 10, 12));
-		toolbar.setSpacing(10);
-		toolbar.setPrefWidth(500);
-		toolbar.getChildren().addAll(pauseButt);
-	}
+	
 	
 	/**
 	 * Method to create the progress bar
@@ -230,47 +204,16 @@ public class GalleryApp extends Application {
 	 */
 	public void play(Timeline timeline, Button pauseButt) {
 		if (pauseButt.getText().equals("pause")) {// what to do if paused
-			pauseButt.setText("play");
+			pauseButt.setText("Play");
 			timeline.pause();
-		} else if (pauseButt.getText().equals("play")) {// what to do if playing
-			pauseButt.setText("pause");
+		} else if (pauseButt.getText().equals("Play")) {// what to do if playing
+			pauseButt.setText("Pause");
 			timeline.play();
 		}
 	}
 	
-	/**
-	 * Method to convert the url into a string
-	 * 
-	 * @param searchBar the textfield in the toolbar
-	 * @param tile      the GridPane of images
-	 */
-/*
-	public void convertUrl(TextField searchBar, GridPane tile) {
-		input = search.getText();
-		urlTests(input);
-		JsonParser jp = new JsonParser();
-		JsonElement je = jp.parse(reader);
-		JsonObject root = je.getAsJsonObject(); // root of response
-		JsonArray results = root.getAsJsonArray("results"); // "results" array
-		numResults = results.size(); // "results" array size
-		if (numResults < 20) {// if less than 20 show error
-			input = input2;
-			TextField tfield = new TextField(input);
-			Platform.runLater(() -> {
-				Alert alert = new Alert(AlertType.ERROR, "Error");
-				alert.setContentText("Less than 20");
-				alert.show();
-			});
-			convertUrl(tfield, tile);
-			loadImage(array);
-		} else {// if more than 20 call makeArray
-			input2 = input;
-			makeArray(results);
-		}
-	}*/
 	
 	/**
-	 * CONVERT2
 	 * Method to convert the url into a string
 	 * 
 	 * @param searchBar the textfield in the toolbar
@@ -278,26 +221,14 @@ public class GalleryApp extends Application {
 	 */
 
 	public void convertUrl() {
-		//input = search.getText();
-		//urlTests(input);
 		urlTests();
-		/*
-		JsonElement je = JsonParser.parse("https://api.pinterest.com/v3/pidgets/boards/sam6072/Bulletin/pins/");
-		JsonObject root = je.getAsJsonObject();
-		JsonArray results = root.getAsJsonArray("pins");
-		*/
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(reader);
-		JsonObject root = je.getAsJsonObject(); // root of response
-		//Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		//String prettyJsonString = gson.toJson(je);
-		//System.out.println(prettyJsonString);
+		JsonObject root = je.getAsJsonObject();			 // root of response
 		root = root.getAsJsonObject("data");
 		JsonArray results = root.getAsJsonArray("pins"); // "results" array
-
-		System.out.println(results.size());
-		numResults = results.size(); // "results" array size
-		if (numResults < 20) {// if less than 20 show error
+		numResults = results.size(); 					 // "results" array size
+		if (numResults < 20) {							 // if less than 20 show error
 			input = input2;
 			TextField tfield = new TextField("filler");
 			Platform.runLater(() -> {
@@ -312,6 +243,7 @@ public class GalleryApp extends Application {
 			makeArray(results);
 		}
 	}
+	
 	/**
 	 * Method to begin converting the url to string and test if it is valid
 	 * 
@@ -319,14 +251,6 @@ public class GalleryApp extends Application {
 	 */
 	
 	public void urlTests() {
-		/*try { // try catch statements to make sure url is valid
-			input = URLEncoder.encode(input, "UTF-8");
-		} catch (Exception e) {
-			System.out.println("Invalid url");
-		}
-		String link = "https://itunes.apple.com/search?term=";
-		String sUrl = link + input + "&entity=album";
-		*/
 		String sUrl = "https://api.pinterest.com/v3/pidgets/boards/sam6072/Bulletin/pins/";
 		URL url = null;
 		try {
@@ -339,37 +263,9 @@ public class GalleryApp extends Application {
 		} catch (Exception e) {
 			System.out.println("Invalid url");
 		}
-
 		
 	}
 	
-	/**
-	 * Method to begin converting the url to string and test if it is valid
-	 * 
-	 * @param input the text from the textfield
-	 */
-	/*
-	public void urlTests(String input) {
-		try { // try catch statements to make sure url is valid
-			input = URLEncoder.encode(input, "UTF-8");
-		} catch (Exception e) {
-			System.out.println("Invalid url");
-		}
-		String link = "https://itunes.apple.com/search?term=";
-		String sUrl = link + input + "&entity=album";
-		URL url = null;
-		try {
-			url = new URL(sUrl);
-		} catch (Exception e) {
-			System.out.println("Invalid url");
-		}
-		try {
-			reader = new InputStreamReader(url.openStream());
-		} catch (Exception e) {
-			System.out.println("Invalid url");
-		}
-	}
-*/
 
 	/**
 	 * Method to create an array of distinct string urls.
@@ -384,15 +280,9 @@ public class GalleryApp extends Application {
 			result  = result.getAsJsonObject("images");
 			result = result.getAsJsonObject("237x");
 			JsonElement imageUrl = result.get("url"); 
-			//Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			//String prettyJsonString = gson.toJson(imageUrl);
-			//System.out.println(prettyJsonString);
-			
 			if (imageUrl != null) { // member might not exist
-				//System.out.println("IN LOOP");
 				String artUrl = imageUrl.getAsString(); // get member as string
 				array[i] = artUrl; // art array
-				//System.out.println(artUrl);
 			}
 		} // make distinct
 		array = Arrays.stream(array).filter(e -> e != null).distinct().toArray(String[]::new);
